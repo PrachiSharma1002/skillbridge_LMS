af2dbca7ceaef8d55c96d8910c3d5aff715f1454
PrachiSharma1002 <prachidilipsharma@gmail.com>
2026-05-19 17:39:55 +0530

    skill bridge

--- app.py
+++ app.py
@@ -532,4 +532,5 @@ def course_lessons(course_id):
     return render_template('course_lessons.html', course=course, lessons=lessons)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
